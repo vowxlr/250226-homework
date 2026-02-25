9162f461c627defe7a49bf5aee70c2ce4a78b5e0
vowxlr <vowxlr@gmail.com>
2026-02-25 15:13:47 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,11 +19,33 @@ namespace _260224_аисд_дз
 
             for(int i = 0; i < k; i++)
             {
-                //метод, создающий массив
-                int cnt = 0; //длина созданного массива
-                arrnum1[i][cnt] = 0; //передпем созданный массив
+                int[] arr = CreateArray();
+                Array.Sort(arr);
+                int cnt = arr.Length;
+                arrnum1[i] = arr;
+            }
+            int cnt2 = 0;
+            for (int i = 0; i < arrnum1.Length; i++)
+            {
+                cnt2 += arrnum1[i].Length;
             }
 
+            int[] arrnum1full = new int[cnt2];
+            int ind = 0;
+            for (int i = 0; i < arrnum1.Length; i++)
+            {
+                for (int j = 0; j < arrnum1[i].Length; j++)
+                {
+                    arrnum1full[ind] = arrnum1[i][j];
+                    ind++;
+                }
+            }
+            arrnum1full = BubbleSort(arrnum1full);
+
+            foreach(var j in arrnum1full)
+            {
+                Console.WriteLine(j);
+            }
             //------------------------
             Console.WriteLine("#2");
             Console.WriteLine("Напишите числа через пробел: ");
@@ -44,6 +66,23 @@ namespace _260224_аисд_дз
             Console.WriteLine($"Ответ: {s}");
 
         }
+        static int[] BubbleSort(int [] arrnum1full)
+        {
+            int temp = 0;
+            for (int i = 0; i < arrnum1full.Length - 1; i++)
+            {
+                for (int j = 0; j < arrnum1full.Length - i - 1; j++)
+                {
+                    if (arrnum1full[j] > arrnum1full[j + 1])
+                    {
+                        temp = arrnum1full[j];
+                        arrnum1full[j] = arrnum1full[j + 1];
+                        arrnum1full[j + 1] = temp;
+                    }
+                }
+            }
+            return arrnum1full;
+        }
         static int IsInt()
         {
             while(true)
@@ -54,6 +93,33 @@ namespace _260224_аисд_дз
                     return newn;
                 }
             }
+        }
+        static int[] CreateArray()
+        {
+            while(true)
+            {
+                Console.WriteLine("Введите числа через пробел: ");
+                string[] n = Console.ReadLine().Trim().Split();
+                bool flag = true;
+                foreach (string s in n)
+                {
+                    if (!int.TryParse(s, out int newn))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    int[] arr = new int[n.Length];
+                    for(int i = 0;i < n.Length;i++)
+                    {
+                        arr[i] = int.Parse(n[i]);
+                    }
+                    return arr;
+                }
+            }
+
         }
         static string[] IsIntArr()
         {
